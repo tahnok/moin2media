ac65a64ec97f3b3b3a203912ae963a18aa7b5770
Wesley <tahnok881@hotmail.com>
2011-02-17 17:05:45 -0500

    v 1!

---
+++ moin2media.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl
+
+#TODO make the math workt with $ in between
+#TODO fix links
+#TODO fix mixed bulleted/numbered list structure
+#TODO date removal...
+#TODO tables...
+#TODO lists with a. in them
+
+open(HANDLE, "moin.txt");
+@testlines = <HANDLE>;
+close HANDLE;
+
+foreach (@testlines) {
+#	while (s/[*#]([*#]+)/ $1/g) {}
+	while (s/^\s*:/ /g) {}
+	s/^(\s*)\*/'*' x length($1)/eg;
+	s/^(\s*)\d\./'#' x (length($1))/eg;
+	s/^{{{/<syntaxhighlight lang = "text">/g;
+	s/^{{{(.*)/<syntaxhighlight lang = "$1">/g;
+	s/^}}}/<\/syntaxhighlight>/g;
+	s/<<latex\(\$(.*?)\$\)>>/<math>$1<\/math>/g;
+	s/ (.*)::(.*)/; $1 : $2/g;
+#	s/[[(.*) | (.*)]]/[$1 $2]/g;
+#	s/^ (.*)/:$1/g;	
+}
+
+open(HANDLES, ">media.txt");
+print HANDLES "$_" foreach @testlines;
+close HANDLES;
+
+
