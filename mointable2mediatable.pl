2de1f42b8c2f6b7cd90240c1c01692bf65a38093
Wesley Ellis <tahnok@gmail.com>
2011-09-13 22:54:42 -0400

    Now with support for tables! Though you do have to use a seperate script...

---
+++ mointable2mediatable.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+
+#TODO make the math workt with $ in between
+#TODO fix links
+#TODO fix mixed bulleted/numbered list structure
+#TODO date removal...
+#TODO tables...
+#TODO lists with a. in them
+
+open(HANDLE, "moin.txt");
+@testlines = <HANDLE>;
+close HANDLE;
+
+$testlines[0] =~ s/\|\| (.*)/\{\|\n\|$1/g;
+$testlines[-1] =~ s/(.*) \|\|/$1\n\|\}/g;
+foreach (@testlines) {
+	s/(.*)\|\|$/$1\n\|-/g;
+	s/^\|\| (.*)/\|$1/g;
+	s/ \|\| /\n\|/g;
+}
+
+open(HANDLES, ">media.txt");
+print HANDLES "$_" foreach @testlines;
+close HANDLES;
+
+
